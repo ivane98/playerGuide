ff9b8444e6f4188512b19ee577f2100b3500b0be
ivane98 <i.vardoshvili22@gmail.com>
2025-05-05 08:43:07 +0400

    var

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿Console.WriteLine("Hello " + "World");
-
+﻿
 string name;
-Console.WriteLine("What is your name: ");
+
 name = Console.ReadLine();
 Console.WriteLine("Hi " + name);
 
+int a, b, c;
+
+a = b = c = 10;
--- obj/Debug/net9.0/playerGuide.AssemblyInfo.cs
+++ obj/Debug/net9.0/playerGuide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("playerGuide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7480790f28013568f9b027ef84ce83e7016195ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("playerGuide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("playerGuide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
